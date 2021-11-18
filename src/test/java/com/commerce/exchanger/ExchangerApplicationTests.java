af534cac6a2eb7f1004110fc2634ab2528007fe7
kamil.serafinowicz <kamil.serafinowicz@plus.pl>
2021-11-18 13:18:24 +0100

    Added project base

---
+++ src/test/java/com/commerce/exchanger/ExchangerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.commerce.exchanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangerApplicationTests {
+
+  @Test
+  void contextLoads() {
+    
+  }
+
+}
