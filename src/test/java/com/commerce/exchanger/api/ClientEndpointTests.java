68fed5ff641ba97be464af8f9285aafb9c1abb83
kamil.serafinowicz <kamil.serafinowicz@plus.pl>
2021-11-29 13:59:38 +0100

    Test typo fix

--- src/test/java/com/commerce/exchanger/api/ClientEndpointTests.java
+++ src/test/java/com/commerce/exchanger/api/ClientEndpointTests.java
@@ -14,7 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class ClientEndpointeTests {
+class ClientEndpointTests {
 
   @Autowired
   private MockMvc mockMvc;
